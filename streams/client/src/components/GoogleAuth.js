25829d1cd0c2c65c472d552833cb64cfece5c2e2
prsheth <pranayrsheth@gmail.com>
2020-05-14 23:38:06 -0400

    added dispatchProps in google auth component and added fixed action types to avoid any string errors through the application

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -58,4 +58,8 @@ class GoogleAuth extends React.Component {
 const mapStateToProps = (state) => {
   return { isSignedIn: state.auth.isSignedIn };
 };
-export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
+const mapDispatchToProps = {
+  signIn,
+  signOut,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(GoogleAuth);
