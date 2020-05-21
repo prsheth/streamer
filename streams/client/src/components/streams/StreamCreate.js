36b2a4d1ecfb846d25d75a3a848d776abe6be06d
prsheth <pranayrsheth@gmail.com>
2020-05-21 08:11:37 -0400

    Wired up redux-from library to manage formdata with the redux store

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <form>
+          <Field name="titleOfStream" />
+          <Field name="descriptionOfStream" />
+        </form>
+      </div>
+    );
+  }
+}
 
-export default StreamCreate;
+//The redux form function is just like connect from react redux but let the library do that for us
+//Similar to mapStateToProps where all props are now preloaded from the library
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import { reducer as formReducer } from "redux-form"; //named import for simplicity and reducing confusion
 
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
