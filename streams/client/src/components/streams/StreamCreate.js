5ad239b0aeb04b957cae44e529549b9821add721
prsheth <pranayrsheth@gmail.com>
2020-06-30 07:27:40 -0400

    Used redux form to wire up form to create a new stream, added validate methods to look for blank fields, added submit handler to manage form submissions

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,23 +2,68 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
-    return <input {...formProps.input} />;
-  }
+  renderInput = ({ input, label, meta }) => {
+    //touched is equivalent to onfocus
+    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        <div>{this.renderError(meta)}</div>
+      </div>
+    );
+  };
+  renderError = ({ touched, error }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
   render() {
     return (
       <div>
-        <form>
-          <Field name="titleOfStream" component={this.renderInput} />
-          <Field name="descriptionOfStream" component={this.renderInput} />
+        <form
+          onSubmit={this.props.handleSubmit(this.onSubmit)} //prop from redux forms and then pass callback method to it
+          className="ui form error"
+        >
+          <Field
+            name="titleOfStream"
+            component={this.renderInput}
+            label="Enter title"
+          />
+          <Field
+            name="descriptionOfStream"
+            component={this.renderInput}
+            label="Enter description"
+          />
+          <button className="ui button primary"> Submit </button>
         </form>
       </div>
     );
   }
 }
+//Validate method
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.titleOfStream) {
+    //redux form expects an object with key and value pair to show validation values, an empty object means no error
+    errors.titleOfStream = "Please enter a valid title";
+  }
+  if (!formValues.descriptionOfStream) {
+    errors.descriptionOfStream = "Please enter a valid description";
+  }
+  return errors;
+};
 
 //The redux form function is just like connect from react redux but let the library do that for us
 //Similar to mapStateToProps where all props are now preloaded from the library
 export default reduxForm({
   form: "streamCreate",
+  validate,
 })(StreamCreate);
