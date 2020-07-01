261e28d7d98141050d1ea4542b06b77f29bdd745
prsheth <pranayrsheth@gmail.com>
2020-07-01 19:14:14 -0400

    created modal component using react portal so that is attached to a div which is a sibling of root, making sure this componenet is customizable by allowing props to change all content, added new dic to index html file and added link to delete route from StreamList component

---
+++ streams/client/src/components/streams/Modal.js
@@ -0,0 +1,28 @@
+//using React portals for this modal
+
+import React from "react";
+import ReactDOM from "react-dom";
+import history from "../../history";
+
+const Modal = (props) => {
+  return ReactDOM.createPortal(
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()} //make sure the above event deosn't get propogated to all children
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
+      </div>
+    </div>,
+    document.querySelector("#modal")
+    //PORTAL is used so that is component can be ported to another location breaking from the nested compoenent herirarchy of React
+    // this div is a sibling to root and a child of body
+  );
+};
+
+export default Modal;
--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Modal from "./Modal";
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  const actions = (
+    // does nothing to html just confirms to guideline to add containing element or <> and </>
+    <React.Fragment>
+      <button className="ui button">Cancel</button>
+      <button className="ui button negative">Delete</button>
+    </React.Fragment>
+  );
+  return (
+    <div>
+      <Modal
+        title="Delete a Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
