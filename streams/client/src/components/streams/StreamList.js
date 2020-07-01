6637acb739dd33d1ba99025f653f820eedcb436c
prsheth <pranayrsheth@gmail.com>
2020-07-01 13:56:13 -0400

    Built basic list stream component to make sure all streams are visible

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera"></i>
+          <div className="content">
+            <div className="header">{stream.titleOfStream}</div>
+            <div className="description">{stream.descriptionOfStream}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui relaxed divided list">{this.renderList()}</div>
+      </div>
+    );
+  }
+}
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) };
 };
-
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
