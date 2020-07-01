2bc2d8030927a3f30257a12239f30964bc9037e9
prsheth <pranayrsheth@gmail.com>
2020-07-01 11:27:30 -0400

    added all CRUD stream operation and reducers to enable updating state

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,5 +1,13 @@
 import { API } from "../apis/streams";
-import { SIGN_OUT, SIGN_IN } from "./types";
+import {
+  SIGN_OUT,
+  SIGN_IN,
+  CREATE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -14,8 +22,38 @@ export const signOut = () => {
   };
 };
 
+//ALL CRUD OPERATION FOR STREAMS
 export const createStream = (formValues) => {
   return async (dispatch) => {
-    API().post("/streams", formValues);
+    const response = await API().post("/streams", formValues);
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+  };
+};
+
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const response = await API().get(`/stream/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+  };
+};
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await API().get("/streams");
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+  };
+};
+
+export const editStream = (id, formValues) => {
+  return async (dispatch) => {
+    const response = await API().put(`/stream/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await API().delete(`/stream/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id });
   };
 };
